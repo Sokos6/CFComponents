633cdb3efad37f59bf1b7c42337484b2e93305cc
Sokos6 <will.sokolowski@gmail.com>
2020-09-03 23:36:12 -0500

    conditional rendering

--- src/Message.js
+++ src/Message.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
 const Message = (props) => {
-  return (
-    <h4 className='bg-success text-white text-center p-2'>
-     {props.greeting}, {props.name}
-    </h4>
-  )
-}
+  if (props.name === 'Will') {
+    return (
+      <h4 className='bg-warning p-2'>
+        {props.greeting}, {props.name}
+      </h4>
+    );
+  } else {
+    return (
+      <h4 className='bg-success text-white text-center p-2'>
+        {props.greeting}, {props.name}
+      </h4>
+    );
+  }
+};
 
 export default Message;
