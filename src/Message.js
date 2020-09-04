ed4e730140c8df8ca4246727478353190b6274aa
Sokos6 <will.sokolowski@gmail.com>
2020-09-04 12:40:09 -0500

    switch

--- src/Message.js
+++ src/Message.js
@@ -1,19 +1,22 @@
 import React from 'react';
 
 const Message = (props) => {
-  if (props.name === 'Will') {
-    return (
-      <h4 className='bg-warning p-2'>
-        {props.greeting}, {props.name}
-      </h4>
-    );
-  } else {
-    return (
-      <h4 className='bg-success text-white text-center p-2'>
-        {props.greeting}, {props.name}
-      </h4>
-    );
+  let classes;
+  switch (props.name) {
+    case 'Will':
+      classes = 'bg-warning p-2';
+      break;
+    case 'Dora':
+      classes = 'bg-secondary text-white text-center p-2';
+      break;
+    default:
+      classes = 'bg-success text-white text-center p-2';
   }
+  return (
+    <h4 className='bg-success text-white text-center p-2'>
+      {props.greeting}, {props.name}
+    </h4>
+  );
 };
 
 export default Message;
